fe3b9bab63c97b093e42fa2e6de5b6c630763156
fayekafateh <111707631+fayekafateh@users.noreply.github.com>
2023-03-24 20:32:55 -0400

    Initial

---
+++ casestudy/case1/README.md
@@ -0,0 +1,2 @@
+# UX_UI_HW_20_CaseStudy1
+Case Study Got To Go App
---
+++ js/index.js
@@ -0,0 +1,54 @@
+// Portfolio
+$(".gtg").hover(function(){
+    $(".gtgbox").css("transform","translateX(100px)")
+    $(".h1gtg").css("transform","translateX(-100px)")
+    $(".gtgButton").fadeIn(3000)
+    $(".gtgimage1").fadeIn(3000)
+    $(".gtgimage2").fadeIn(3000);
+
+})
+
+$(".dAssist").hover(function(){
+    $(".dAssistbox").css("transform","translateX(-100px)")
+    $(".h1dAssist").css("transform","translateX(100px)")
+    $(".dAssistButton").fadeIn(3000)
+    $(".dAssistimage1").fadeIn(3000)
+    $(".dAssistimage2").fadeIn(3000)
+    $(".dAssistimage3").fadeIn(3000);
+
+})
+
+$(".mFerals").hover(function(){
+    $(".mFeralsbox").css("transform","translateX(100px)")
+    $(".h1mFerals").css("transform","translateX(-100px)")
+    $(".mFeralsButton").fadeIn(3000)
+    $(".mFeralsimage1").fadeIn(3000)
+    $(".mFeralsgif1").fadeIn(3000)
+    $(".mFeralsgif2").fadeIn(3000);
+
+})
+
+// Nav-Buttons
+$(".buttonTop").hover(function(){
+    $(".icontagstop").fadeIn(500);},function(){$(".icontagstop").fadeOut(100);}
+);
+
+$(".buttonPort").hover(function(){
+    $(".icontagsPort").fadeIn(500);},function(){$(".icontagsPort").fadeOut(100);}
+);
+
+$(".buttonExp").hover(function(){
+    $(".icontagsexp").fadeIn(500);},function(){$(".icontagsexp").fadeOut(100);}
+);
+
+$(".buttonSkl").hover(function(){
+    $(".icontagsSkl").fadeIn(500);},function(){$(".icontagsSkl").fadeOut(100);}
+);
+
+$(".buttonAbt").hover(function(){
+    $(".icontagsabt").fadeIn(500);},function(){$(".icontagsabt").fadeOut(100);}
+);
+
+$(".buttonCon").hover(function(){
+    $(".icontagscon").fadeIn(500);},function(){$(".icontagscon").fadeOut(100);}
+);
